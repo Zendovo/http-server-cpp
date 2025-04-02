f622960b8efd749efe9a8a645e105f9b6387b504
Himanshu Kumar <kumarh664@gmail.com>
2025-04-02 19:34:13 +0530

    user agent path

--- src/server.cpp
+++ src/server.cpp
@@ -8,10 +8,19 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-struct request {
+struct headers_t {
+  std::string user_agent;
+  std::string accept;
+  std::string host;
+  std::string content_type;
+  uint16_t content_length;
+};
+
+struct request_t {
   std::string method;
   std::string path;
   std::string version;
+  headers_t headers;
 };
 
 int main(int argc, char **argv) {
@@ -74,27 +83,36 @@ int main(int argc, char **argv) {
   buffer[bytes_received] = '\0'; // Null-terminate the received data
   std::cout << "Received data from client:\n" << buffer << "\n";
 
-  request req;
+  request_t req;
+  headers_t req_header;
 
   std::string str(buffer);
   size_t pos = str.find('\r\n');
   std::string req_data = str.substr(0, pos);
+  std::string header_data = str.substr(pos + 1, str.rfind('\r\n') - 1);
 
   req.method = req_data.substr(0, req_data.find(' '));
   req.path = req_data.substr(req_data.find(' ') + 1, req_data.rfind(' ') - req_data.find(' ') - 1);
   req.version = req_data.substr(req_data.rfind(' ') + 1);
+  req.headers = req_header;
+
+  req.headers.user_agent = header_data.substr(header_data.find("User-Agent") + 12);
+  req.headers.user_agent = req.headers.user_agent.substr(0, req.headers.user_agent.find('\r'));
 
   std::string response = "HTTP/1.1 404 Not Found\r\n\r\n";
 
   if (req.path == "/") {
     response = "HTTP/1.1 200 OK\r\n\r\n";
   } else if (req.path.substr(1, req.path.substr(1).find('/')) == "echo") {
-
     // Extract the string to echo from the path
     std::string echo_string = req.path.substr(req.path.substr(1).find('/') + 2);
 
     char buffer[1024];
     sprintf(buffer, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%zu\r\n\r\n%s", echo_string.length(), echo_string.c_str());
+    response = buffer;
+  } else if (req.path.substr(1, req.path.substr(1).find('/')) == "user-agent") {
+    char buffer[1024];
+    sprintf(buffer, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%zu\r\n\r\n%s", req.headers.user_agent.length(), req.headers.user_agent.c_str());
     response = buffer;
   }
   
