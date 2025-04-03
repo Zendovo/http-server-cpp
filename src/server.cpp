fbae6e41614ffe36228c37146a4e1f7f12bea7e1
Himanshu Kumar <kumarh664@gmail.com>
2025-04-03 15:01:58 +0530

    multiple requests

--- src/server.cpp
+++ src/server.cpp
@@ -7,8 +7,10 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <thread>
 
-struct headers_t {
+struct headers_t
+{
   std::string user_agent;
   std::string accept;
   std::string host;
@@ -16,109 +18,182 @@ struct headers_t {
   uint16_t content_length;
 };
 
-struct request_t {
+struct request_t
+{
   std::string method;
   std::string path;
   std::string version;
   headers_t headers;
 };
 
-int main(int argc, char **argv) {
+std::string get_response(request_t &req);
+void handle_request(int client, char *buffer);
+void extract_request(char *buffer, request_t &req);
+
+int main(int argc, char **argv)
+{
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  
+
   // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
+  if (server_fd < 0)
+  {
+    std::cerr << "Failed to create server socket\n";
+    return 1;
   }
-  
+
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
+  {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(4221);
-  
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
+  {
     std::cerr << "Failed to bind to port 4221\n";
     return 1;
   }
-  
+
   int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
+  if (listen(server_fd, connection_backlog) != 0)
+  {
     std::cerr << "listen failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
-  
+
   std::cout << "Waiting for a client to connect...\n";
-  
-  int client = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  std::cout << "Client connected\n";
-  if (client < 0) {
-    std::cerr << "Failed to accept client connection\n";
-    return 1;
-  }
-  
-  char buffer[1024];
-  ssize_t bytes_received = recv(client, buffer, sizeof(buffer) - 1, 0);
-  if (bytes_received < 0) {
-    std::cerr << "Failed to receive data from client\n";
-    close(client);
-    return 1;
-  }
-  buffer[bytes_received] = '\0'; // Null-terminate the received data
-  std::cout << "Received data from client:\n" << buffer << "\n";
 
-  request_t req;
-  headers_t req_header;
+  while (1)
+  {
+    // Accept a new client connection
+    int client = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+    std::cout << "Client connected\n";
+    if (client < 0)
+    {
+      std::cerr << "Failed to accept client connection\n";
+      return 1;
+    }
 
-  std::string str(buffer);
-  size_t pos = str.find('\r\n');
-  std::string req_data = str.substr(0, pos);
-  std::string header_data = str.substr(pos + 1, str.rfind('\r\n') - 1);
-
-  req.method = req_data.substr(0, req_data.find(' '));
-  req.path = req_data.substr(req_data.find(' ') + 1, req_data.rfind(' ') - req_data.find(' ') - 1);
-  req.version = req_data.substr(req_data.rfind(' ') + 1);
-  req.headers = req_header;
-
-  req.headers.user_agent = header_data.substr(header_data.find("User-Agent") + 12);
-  req.headers.user_agent = req.headers.user_agent.substr(0, req.headers.user_agent.find('\r'));
-
-  std::string response = "HTTP/1.1 404 Not Found\r\n\r\n";
-
-  if (req.path == "/") {
-    response = "HTTP/1.1 200 OK\r\n\r\n";
-  } else if (req.path.substr(1, req.path.substr(1).find('/')) == "echo") {
-    // Extract the string to echo from the path
-    std::string echo_string = req.path.substr(req.path.substr(1).find('/') + 2);
-
+    // Store the received data in a buffer
     char buffer[1024];
-    sprintf(buffer, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%zu\r\n\r\n%s", echo_string.length(), echo_string.c_str());
-    response = buffer;
-  } else if (req.path.substr(1, req.path.substr(1).find('/')) == "user-agent") {
-    char buffer[1024];
-    sprintf(buffer, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%zu\r\n\r\n%s", req.headers.user_agent.length(), req.headers.user_agent.c_str());
-    response = buffer;
+    ssize_t bytes_received = recv(client, buffer, sizeof(buffer) - 1, 0);
+    if (bytes_received < 0)
+    {
+      std::cerr << "Failed to receive data from client\n";
+      close(client);
+      return 1;
+    }
+    buffer[bytes_received] = '\0';
+    std::cout << "Received data from client:\n"
+              << buffer << "\n";
+
+    std::thread th = std::thread(handle_request, client, buffer);
+    th.detach();
   }
-  
-  send(client, response.c_str(), response.length(), 0);
-  
+
   close(server_fd);
 
   return 0;
+}
+
+void handle_request(int client, char *buffer)
+{
+  char *buffer_copy = new char[1024];
+  std::strcpy(buffer_copy, buffer);
+  request_t req;
+  extract_request(buffer_copy, req);
+
+  std::string response = get_response(req);
+
+  send(client, response.c_str(), response.length(), 0);
+  delete[] buffer_copy;
+}
+
+void extract_request(char *buffer, request_t &req)
+{
+  headers_t req_header;
+
+  std::string str(buffer);
+  size_t pos = str.find("\r\n");
+  std::string req_data = str.substr(0, pos);
+  std::string header_data = str.substr(pos + 2);
+
+  size_t first_space = req_data.find(' ');
+  size_t last_space = req_data.rfind(' ');
+
+  req.method = req_data.substr(0, first_space);
+
+  if (first_space != std::string::npos && last_space != std::string::npos && first_space != last_space)
+  {
+    req.path = req_data.substr(first_space + 1, last_space - first_space - 1);
+    req.version = req_data.substr(last_space + 1);
+  }
+  else
+  {
+    // Handle the case where there are not enough spaces
+    req.path = "/";
+    req.version = "HTTP/1.1";
+  }
+  req.headers = req_header;
+
+  size_t user_agent_pos = header_data.find("User-Agent");
+  if (user_agent_pos != std::string::npos)
+  {
+    req.headers.user_agent = header_data.substr(user_agent_pos + 12);
+    req.headers.user_agent = req.headers.user_agent.substr(0, req.headers.user_agent.find('\r'));
+  }
+  else
+  {
+    req.headers.user_agent = "";
+  }
+}
+
+std::string get_response(request_t &req)
+{
+  try
+  {
+    std::string response = "HTTP/1.1 404 Not Found\r\n\r\n";
+
+    if (req.path == "/")
+    {
+      response = "HTTP/1.1 200 OK\r\n\r\n";
+    }
+    else if (req.path.substr(1, req.path.substr(1).find('/')) == "echo")
+    {
+      // Extract the string to echo from the path
+      std::string echo_string = req.path.substr(req.path.substr(1).find('/') + 2);
+
+      char buffer[1024];
+      sprintf(buffer, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%zu\r\n\r\n%s", echo_string.length(), echo_string.c_str());
+      response = buffer;
+    }
+    else if (req.path.substr(1, req.path.substr(1).find('/')) == "user-agent")
+    {
+      char buffer[1024];
+      sprintf(buffer, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%zu\r\n\r\n%s", req.headers.user_agent.length(), req.headers.user_agent.c_str());
+      response = buffer;
+    }
+
+    return response;
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << e.what() << '\n';
+    return "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+  }
 }
